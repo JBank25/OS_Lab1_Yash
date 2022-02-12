1dfb3037afbf7681bdcc812035296bd7c1f39014
JBank25 <44713040+JBank25@users.noreply.github.com>
2022-02-12 12:22:10 -0600

    Not working but trying to add jobs to LL

--- main.c
+++ main.c
@@ -12,8 +12,21 @@
 #define MAX_CHARS 2000
 #define MAX_TOKENS 30
 
+typedef enum jobStatus {STOPPED , RUNNING}status_t;
+typedef struct job {
+    char jobStr[80];//name of the job as it was run ./looper 1 & for example
+    int pgid;//process group ID
+    status_t status;//status of job stopped or running
+    struct job * next;
+} job_t;
+job_t * jobList;//will be LL used to track all jobs
+job_t * mostRecentJob;
 
+int shell;      
+pid_t shellPid;
 char ** parseInput(char * input);
+void createJob(char * parsedCommand);
+void printJobs();
 
 int main(int argc, char** argv)
 {
@@ -24,15 +37,30 @@ int main(int argc, char** argv)
     char ** redirectCommands = (char**)malloc(scaler*3);
     int redirectionReturnCheck;
     int pipeIndex = 0;
-     
+
+    shellPid = getpid();//we'll initially get the process ID of our shell
+    setpgid(shellPid,shellPid);//set the group ID of the shell
+    job_t *headJob = malloc(sizeof(job_t));
+    mostRecentJob = headJob;
+    jobList = &headJob;
+    headJob->pgid = shellPid;
+    headJob->status = RUNNING;
+    strcpy(headJob->jobStr, "SHELL");
+
+    printf("Shell ID: %d Shell Group ID: %d\n",shellPid,getpgid(shellPid));
     while(cmd = readline("# "))
     {
+        printf("Most recent Job: %s\n", mostRecentJob->jobStr);
+        char inputHold[MAX_CHARS];
+        strcpy(inputHold, cmd);
         parsedCommand = parseInput(cmd);//parse commands
+        createJob(inputHold);
         pipeIndex = getPipeIndex(parsedCommand);//get index of the pipe if one exists
         cpid = fork();
         if(cpid ==0)
         {
-            redirectionReturnCheck = redirectionCheck(parsedCommand);//see if any redirection commands exist
+            printJobs();
+            redirectionReturnCheck = 0;//redirectionCheck(parsedCommand);//see if any redirection commands exist
             if(redirectionReturnCheck)
             {
                 if(pipeIndex==0)
@@ -46,7 +74,8 @@ int main(int argc, char** argv)
                     setRedirection(commandsAfterPipe);
                 }
             }
-            execvp(parsedCommand[0], parsedCommand);
+            else
+                execvp(parsedCommand[0], parsedCommand);
         }
         else
         {
@@ -55,8 +84,32 @@ int main(int argc, char** argv)
     }
     free(parsedCommand);
     free(redirectCommands);
+    free(headJob);
 }
 
+void printJobs()
+{
+    job_t *headJob = jobList;
+    while(jobList != NULL)
+    {
+        printf("Job: %s Status: %d\n", jobList->jobStr, jobList->status);
+        jobList = jobList->next;
+    }
+    jobList = headJob;
+}
+
+void createJob(char * command)
+{
+    printf("Create Job for : %s\n", command);
+    job_t *newJob = (job_t*) malloc(sizeof(job_t));
+    strcpy(newJob->jobStr, command);
+    newJob->status = STOPPED;
+    mostRecentJob->next = &newJob;
+    mostRecentJob = &newJob;
+    mostRecentJob->next = NULL;
+    printf("New Job: %s Job status: %d\n", newJob->jobStr, newJob->status);
+    printf("sucess\n");
+}
 char ** parseInput(char * input)
 {
     char * token = strtok(input, " ");
